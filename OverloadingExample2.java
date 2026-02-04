9458f28080266c3ed3f5b1327ac4f5b2caaa9f84
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:25:30 +0700

    Add overloaded multiply methods for int and double

---
+++ OverloadingExample2.java
@@ -0,0 +1,10 @@
+package lec08_objects_classes.method_overloading;
+
+public class OverloadingExample2 {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
