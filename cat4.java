208d8ae27966d11dd2b33595aac874e69b96df63
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:45:05 +0700

    Implement Cat class with overridden makeSound method

---
+++ cat4.java
@@ -0,0 +1,7 @@
+package LEC9_InheritaceAndPolymorphsm.Super.Runtimepolymorphism;
+class Cat extends Animal {
+ @Override
+ void makeSound() {
+ System.out.println("meo meo meo meo meo con meo ngu ngoc dang yeu cute pho mai que cua chi");
+ }
+}
