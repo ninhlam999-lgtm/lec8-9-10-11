f1afd1b9f64417b32d02fcf218ef624fe97ffab9
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:40:43 +0700

    Add Processor2 class with main method

---
+++ Processor2.java
@@ -0,0 +1,8 @@
+package LEC9_InheritaceAndPolymorphsm.Super.InstanceVarable;
+
+public class Processor {
+ public static void main(String[] args) {
+Husky husky = new Husky();
+husky.displayPrice();
+}
+}
