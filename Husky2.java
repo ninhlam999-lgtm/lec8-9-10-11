339f668735c387a3cc1b96c7d0cb525178d7bf90
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:40:12 +0700

    Add Husky class extending Dog with price display

---
+++ Husky2.java
@@ -0,0 +1,11 @@
+package LEC9_InheritaceAndPolymorphsm.Super.InstanceVarable;
+
+public class Husky extends Dog {
+
+    int price = 1500;
+
+    void displayPrice() {
+        System.out.println("Dog's price: " + super.price);
+        System.out.println("Husky's price: " + price);
+    }
+}
