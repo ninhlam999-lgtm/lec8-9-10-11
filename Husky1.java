0c3cb50d23a668d47441189d9de70acec98711c4
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:38:02 +0700

    Add Husky class extending Dog with constructor

---
+++ Husky1.java
@@ -0,0 +1,9 @@
+package LEC9_InheritaceAndPolymorphsm.Super.Constructor;
+
+public class Husky extends Dog {
+
+    Husky() {
+        super();
+        System.out.println("Husky's constructor is invoked");
+    }
+}
