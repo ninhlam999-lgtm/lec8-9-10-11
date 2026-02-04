3e5b86edb6e07e492ca73a47d1dc083b9f2e1f71
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:29:45 +0700

    Add Processor class to display car details

---
+++ Processor2.java
@@ -0,0 +1,8 @@
+package lec08_objects_classes.object_class;
+
+public class Processor {
+    public static void main(String[] args) {
+        Car myCar = new Car("Lamborghini", 2020);
+        myCar.displayDetails();
+    }
+}
