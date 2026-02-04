b0a5ceeeaa75ff9384970f38968d59028f3ce838
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:42:29 +0700

    Add Huskys class extending Dog with display method

---
+++ Husky3.java
@@ -0,0 +1,7 @@
+package LEC9_InheritaceAndPolymorphsm.Super.Method;
+
+public class Huskys extends Dog {
+    void displayInformation() {
+        super.displayPrice();
+        System.out.println("Husky's price is 1500 USD");
+    }}
