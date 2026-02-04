0acb206001586bc1fb776fd303b83d5a7083f709
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:59:35 +0700

    Update and rename 0 to Processor3.java

---
+++ Processor3.java
@@ -0,0 +1,7 @@
+package lec10_abstraction.abstractclass.Shape_NonAbstractMethod;
+public class Processor {
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.displayInfo();
+    }
+}
