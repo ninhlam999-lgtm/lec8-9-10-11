21ef028246d6cdc2bbfcfa7d47636626c87cea2a
ninhlam999-lgtm <ninhlam999@gmail.com>
2026-02-05 04:24:51 +0700

    Add method overloading for addition in OverloadingExample1

---
+++ OverloadingExample1.java
@@ -0,0 +1,10 @@
+package lec08_objects_classes.method_overloading;
+
+public class OverloadingExample1 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
